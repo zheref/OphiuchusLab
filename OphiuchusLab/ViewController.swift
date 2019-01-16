6895ff2689f03d464ad9c761703af16a0f48672c
Sergio Daniel Lozano <me@sergiodaniel.pro>
2019-01-16 18:05:21 -0500

    Initial Commit

---
+++ OphiuchusLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OphiuchusLab
+//
+//  Created by Sergio Daniel on 1/16/19.
+//  Copyright © 2019 Sergio Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
